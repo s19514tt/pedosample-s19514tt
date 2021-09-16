44ed435b0ef3a3c328c83fa875a40bb6f106f638
s19514tt <s19514tt@sfc.keio.ac.jp>
2021-09-16 23:06:36 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:awareframework_accelerometer/awareframework_accelerometer.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  AccelerometerSensor sensor;
+  AccelerometerSensorConfig config;
+  AccelerometerCard card;
+
+  bool sensorState = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    config = AccelerometerSensorConfig()
+      ..debug = true
+      ..label = "label"
+      ..frequency = 100;
+
+    // init sensor without a context-card
+    sensor = new AccelerometerSensor.init(config);
+    sensor.start();
+    sensor.onDataChanged.listen((data){
+      print(data);
+    });
+
+    // init sensor with card
+    card = new AccelerometerCard(sensor: sensor, bufferSize: 299,);
+    // card = new AccelerometerCard(sensor: sensor,);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Plugin Example App'),
+          ),
+          body: card
+      ),
+    );
+  }
+
+
+}
